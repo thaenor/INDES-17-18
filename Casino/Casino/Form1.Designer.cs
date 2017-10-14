b51a0c52cf54dabcb924806a5e072a77b4704851
Francisco Santos <1111315@isep.ipp.pt>
2017-10-14 22:30:26 +0100

    added casino project which had some git messup that is now clean

---
+++ Casino/Casino/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Casino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FirstNumber = new System.Windows.Forms.Label();
+            this.SecondNumber = new System.Windows.Forms.Label();
+            this.ThirdNumber = new System.Windows.Forms.Label();
+            this.MoneyLbl = new System.Windows.Forms.Label();
+            this.ResultDisplay = new System.Windows.Forms.Label();
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.CurrentMoney = new System.Windows.Forms.Label();
+            this.CurrentBet = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentBet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(305, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Place your bet (€):";
+            // 
+            // FirstNumber
+            // 
+            this.FirstNumber.AutoSize = true;
+            this.FirstNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.FirstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNumber.Location = new System.Drawing.Point(315, 270);
+            this.FirstNumber.Name = "FirstNumber";
+            this.FirstNumber.Size = new System.Drawing.Size(100, 110);
+            this.FirstNumber.TabIndex = 1;
+            this.FirstNumber.Text = "0";
+            this.FirstNumber.Visible = false;
+            // 
+            // SecondNumber
+            // 
+            this.SecondNumber.AutoSize = true;
+            this.SecondNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SecondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondNumber.Location = new System.Drawing.Point(468, 270);
+            this.SecondNumber.Name = "SecondNumber";
+            this.SecondNumber.Size = new System.Drawing.Size(100, 110);
+            this.SecondNumber.TabIndex = 2;
+            this.SecondNumber.Text = "0";
+            this.SecondNumber.Visible = false;
+            this.SecondNumber.Click += new System.EventHandler(this.SecondNumber_Click);
+            // 
+            // ThirdNumber
+            // 
+            this.ThirdNumber.AutoSize = true;
+            this.ThirdNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ThirdNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThirdNumber.Location = new System.Drawing.Point(621, 270);
+            this.ThirdNumber.Name = "ThirdNumber";
+            this.ThirdNumber.Size = new System.Drawing.Size(100, 110);
+            this.ThirdNumber.TabIndex = 3;
+            this.ThirdNumber.Text = "0";
+            this.ThirdNumber.Visible = false;
+            // 
+            // MoneyLbl
+            // 
+            this.MoneyLbl.AutoSize = true;
+            this.MoneyLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MoneyLbl.Location = new System.Drawing.Point(12, 554);
+            this.MoneyLbl.Name = "MoneyLbl";
+            this.MoneyLbl.Size = new System.Drawing.Size(328, 67);
+            this.MoneyLbl.TabIndex = 4;
+            this.MoneyLbl.Text = "money (€) :";
+            // 
+            // ResultDisplay
+            // 
+            this.ResultDisplay.AutoSize = true;
+            this.ResultDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultDisplay.Location = new System.Drawing.Point(12, 621);
+            this.ResultDisplay.Name = "ResultDisplay";
+            this.ResultDisplay.Size = new System.Drawing.Size(0, 33);
+            this.ResultDisplay.TabIndex = 5;
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayBtn.Location = new System.Drawing.Point(315, 433);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(406, 71);
+            this.PlayBtn.TabIndex = 6;
+            this.PlayBtn.Text = "Play";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // CurrentMoney
+            // 
+            this.CurrentMoney.AutoSize = true;
+            this.CurrentMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentMoney.Location = new System.Drawing.Point(346, 554);
+            this.CurrentMoney.Name = "CurrentMoney";
+            this.CurrentMoney.Size = new System.Drawing.Size(95, 67);
+            this.CurrentMoney.TabIndex = 7;
+            this.CurrentMoney.Text = "20";
+            // 
+            // CurrentBet
+            // 
+            this.CurrentBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentBet.Location = new System.Drawing.Point(315, 165);
+            this.CurrentBet.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.CurrentBet.Name = "CurrentBet";
+            this.CurrentBet.Size = new System.Drawing.Size(406, 62);
+            this.CurrentBet.TabIndex = 8;
+            this.CurrentBet.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Casino.Properties.Resources.apple1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(315, 270);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 110);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Casino.Properties.Resources.apple1;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(468, 270);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 110);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::Casino.Properties.Resources.apple1;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(621, 270);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 110);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 697);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CurrentBet);
+            this.Controls.Add(this.CurrentMoney);
+            this.Controls.Add(this.PlayBtn);
+            this.Controls.Add(this.ResultDisplay);
+            this.Controls.Add(this.MoneyLbl);
+            this.Controls.Add(this.ThirdNumber);
+            this.Controls.Add(this.SecondNumber);
+            this.Controls.Add(this.FirstNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentBet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FirstNumber;
+        private System.Windows.Forms.Label SecondNumber;
+        private System.Windows.Forms.Label ThirdNumber;
+        private System.Windows.Forms.Label MoneyLbl;
+        private System.Windows.Forms.Label ResultDisplay;
+        private System.Windows.Forms.Button PlayBtn;
+        private System.Windows.Forms.Label CurrentMoney;
+        private System.Windows.Forms.NumericUpDown CurrentBet;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Casino/Casino/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casino
+{
+    public partial class Form1 : Form
+    {
+        int n1=0, n2=0, n3=0, counter = 30;
+        decimal bet, money;
+        string message;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SecondNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int getRandom()
+        {
+            return random.Next(1, 9); //9 not included
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                CalculateResults();
+            } else
+            {
+                DisplayFruit(pictureBox1, getRandom());
+                DisplayFruit(pictureBox2, getRandom());
+                DisplayFruit(pictureBox3, getRandom());
+            }
+        }
+
+        private decimal CheckResults()
+        {
+            decimal t = bet; //temporary store the bet for calculations
+            if(n1 == 7 || n2 == 7 || n3 == 7) //at least one result is 7
+            {
+                if ((n1 == 7 && n2 == 7) || (n1 == 7 && n3 == 7) || (n2 == 7 && n3 == 7)) //at least two results are 7
+                {
+                    if (n1 == 7 && n2 == 7 && n3 == 7) //snake eyes! (or whatever) all numbers are starberry
+                    {
+                        message = "Awesome, you got all strawberries! - your bet is tenfold!";
+                        return bet * 10;
+                    }
+
+                    message = "Congratulations, you got two strawberries! - your bet is tripled";
+                    return bet * 3;
+                }
+                message = "Congratulations, you got a starberry! - your bet is doubled";
+                return bet * 2;
+            }
+            if(money == 0)
+            {
+                message = "You lucked out, I'm sorry but it's game over. (restart the app to play again)";
+                PlayBtn.Enabled = false;
+                CurrentBet.Enabled = false;
+            }
+            else
+            {
+                message = "Sorry, none of the fruits is a starberry. Try again.";
+            }
+            return 0;
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            Boolean canPlay = true;
+            
+            if (CurrentBet.Value <= 0)
+            {
+                MessageBox.Show("Your bet needs to be higher than 0.");
+                canPlay = false;
+            }
+
+            if(CurrentBet.Value > Int32.Parse(CurrentMoney.Text))
+            {
+                MessageBox.Show("You can't bet more than you have");
+                canPlay = false;
+            }
+
+            if (canPlay)
+            {
+                timer1.Start();
+                timer2.Start();
+                timer3.Start();
+            }
+        }
+
+        private void CalculateResults()
+        {
+            money = Convert.ToDecimal(CurrentMoney.Text);
+            bet = CurrentBet.Value;
+            money = money - bet;
+            //let's rool!
+            n1 = getRandom();
+            n2 = getRandom();
+            n3 = getRandom();
+            //show the results
+            //FirstNumber.Text = n1.ToString();
+            DisplayFruit(pictureBox1, n1);
+            //SecondNumber.Text = n2.ToString();
+            DisplayFruit(pictureBox2, n2);
+            //ThirdNumber.Text = n3.ToString();
+            DisplayFruit(pictureBox3, n3);
+            //calculate the results
+            bet = CheckResults();
+            money += bet;
+            CurrentMoney.Text = money.ToString();
+            ResultDisplay.Text = message;
+        }
+
+        private void DisplayFruit(PictureBox img, int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    img.BackgroundImage = Properties.Resources.apple1;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+                case 2:
+                    img.BackgroundImage = Properties.Resources.orange2;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+                case 3:
+                    img.BackgroundImage = Properties.Resources.watermellon3;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+                case 4:
+                    img.BackgroundImage = Properties.Resources.carrot4;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+                case 5:
+                    img.BackgroundImage = Properties.Resources.pear5;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+                case 6:
+                    img.BackgroundImage = Properties.Resources.grapes6;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+                case 7:
+                    img.BackgroundImage = Properties.Resources.strawberry7;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+                case 8:
+                    img.BackgroundImage = Properties.Resources.banana8;
+                    img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                    break;
+            }
+        }
+    }
+}
