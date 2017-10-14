32e5c9e1c0ee84f03769c55b7a4ff04bb70daab6
Francisco Santos <1111315@isep.ipp.pt>
2017-10-14 22:15:26 +0100

    Created MonoRepo style project. Imported the 3 completed projects.

---
+++ RPSGame/RPSGame/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace RPSGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.playerScorelbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.computerScroreLbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.showResult = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1031, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1031, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Paper";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1031, 468);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Scissors";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(613, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Player";
+            // 
+            // playerScorelbl
+            // 
+            this.playerScorelbl.AutoSize = true;
+            this.playerScorelbl.Location = new System.Drawing.Point(737, 203);
+            this.playerScorelbl.Name = "playerScorelbl";
+            this.playerScorelbl.Size = new System.Drawing.Size(24, 25);
+            this.playerScorelbl.TabIndex = 4;
+            this.playerScorelbl.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Computer";
+            // 
+            // computerScroreLbl
+            // 
+            this.computerScroreLbl.AutoSize = true;
+            this.computerScroreLbl.Location = new System.Drawing.Point(255, 203);
+            this.computerScroreLbl.Name = "computerScroreLbl";
+            this.computerScroreLbl.Size = new System.Drawing.Size(24, 25);
+            this.computerScroreLbl.TabIndex = 6;
+            this.computerScroreLbl.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(347, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Classic Rock Paper Scissors game";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(345, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Rock > Scissors > Paper > Rock ...";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = global::RPSGame.Properties.Resources.waiting;
+            this.pictureBox2.Location = new System.Drawing.Point(618, 274);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(284, 260);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::RPSGame.Properties.Resources.waiting;
+            this.pictureBox1.Location = new System.Drawing.Point(123, 274);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // showResult
+            // 
+            this.showResult.AutoSize = true;
+            this.showResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showResult.Location = new System.Drawing.Point(447, 575);
+            this.showResult.Name = "showResult";
+            this.showResult.Size = new System.Drawing.Size(0, 55);
+            this.showResult.TabIndex = 11;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::RPSGame.Properties.Resources.win;
+            this.pictureBox3.Location = new System.Drawing.Point(956, 554);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(388, 277);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::RPSGame.Properties.Resources.RPS;
+            this.pictureBox4.Location = new System.Drawing.Point(881, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(325, 181);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1405, 872);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.showResult);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.computerScroreLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.playerScorelbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(1431, 943);
+            this.MinimumSize = new System.Drawing.Size(1431, 943);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label playerScorelbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label computerScroreLbl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label showResult;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ RPSGame/RPSGame/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPSGame
+{
+    public partial class Form1 : Form
+    {
+        int playerScore = 0, pcScore = 0;
+        int computerMove = 0, playerMove = 0;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingNavigatorMoveNextItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AIMove()
+        {
+            computerMove = random.Next(1, 4);
+            switch (computerMove){
+                case 1:
+                    pictureBox1.Image = Properties.Resources.PEDRA;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.PAPEL;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.TESOURA;
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Visible = false;
+            playerMove = 2; //Paper
+            pictureBox2.Image = Properties.Resources.PAPEL;
+            AIMove();
+            checkResults();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Visible = false;
+            playerMove = 3; //Scissors
+            pictureBox2.Image = Properties.Resources.TESOURA;
+            AIMove();
+            checkResults();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Visible = false;
+            playerMove = 1; //Rock
+            pictureBox2.Image = Properties.Resources.PEDRA;
+            AIMove();
+            checkResults();
+        }
+
+        private void checkResults()
+        {
+            if (playerMove == 1 && computerMove == 3)
+                playerWins();
+            if (playerMove == 2 && computerMove == 1)
+                playerWins();
+            if (playerMove == 3 && computerMove == 2)
+                playerWins();
+            if (playerMove == 1 && computerMove == 2)
+                AIWins();
+            if (playerMove == 2 && computerMove == 3)
+                AIWins();
+            if (playerMove == 3 && computerMove == 1)
+                AIWins();
+            if (playerMove == computerMove)
+                showResult.Text = "It's a draw!";
+        }
+
+        private void playerWins()
+        {
+            playerScore++;
+            playerScorelbl.Text = playerScore.ToString();
+            showResult.Text = "congrats! you Win!";
+            pictureBox3.Visible = true;
+        }
+
+        private void AIWins()
+        {
+            pcScore++;
+            computerScroreLbl.Text = pcScore.ToString();
+            showResult.Text = "I'm sorry, you lost.";
+        }
+    }
+}
